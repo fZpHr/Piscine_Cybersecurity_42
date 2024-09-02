282618ead3ec7b09006fa5cd846157f6bded5640
fZpHr <afterrush@live.fr>
2024-09-02 16:31:57 +0200

    test

---
+++ Cybersecurity - arachnida - Web /spider.py
@@ -0,0 +1,28 @@
+import requests
+from bs4 import BeautifulSoup
+import shutil
+import os
+import sys
+
+url = sys.argv[1]
+
+response = requests.get(url)
+if response.status_code != 200:
+    print("Erreur lors de la requête.")
+    exit()
+
+soup = BeautifulSoup(response.content, 'html.parser')
+img_tags = soup.find_all('img')
+png_urls = [img['src'] for img in img_tags if 'src' in img.attrs and img['src'].endswith('.png')]
+for i, image_url in enumerate(png_urls):
+
+    if not image_url.startswith(('http://', 'https://')):
+        image_url = os.path.join(url, image_url)
+
+    image_response = requests.get(image_url, stream=True)
+
+    with open(f'image_{i}.png', 'wb') as out_file:
+        shutil.copyfileobj(image_response.raw, out_file)
+
+    print(f"Image {i} téléchargée avec succès. ")ls
+    
